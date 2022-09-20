33a01ac9105953166eddcf9c656b470e130fca93
Mariam <m.surmava@sloboda-studio.com>
2022-09-20 17:51:39 +0400

    upto Releasing Tracked Resources

--- src/controllers/content_loader_controller.js
+++ src/controllers/content_loader_controller.js
@@ -1,10 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = { url: String }
+  static values = { url: String, refreshInterval: Number }
+
+  startRefreshing() {
+    setInterval(() => {
+      this.load()
+    }, this.refreshIntervalValue)
+  }
 
   connect() {
     this.load()
+
+    if (this.hasRefreshIntervalValue) {
+      this.startRefreshing()
+    }
   }
 
   load() {
